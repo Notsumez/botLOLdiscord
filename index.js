e05011a40af7b5177b062847690b91eab8a8fc47
Notsume <adahorapirola@gmail.com>
2024-01-05 10:16:13 -0300

    Criação inicial do bot com funções básicas para seu funcionamento

---
+++ index.js
@@ -0,0 +1,36 @@
+import { Client, IntentsBitField } from "discord.js";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent
+  ]
+});
+
+client.on("messageCreate", (message) => {
+    if(message.author.bot) return;
+
+    if(message.content.startsWith("/lol")){
+        const mentionedUser = message.mentions.users.first();
+
+        if(mentionedUser){
+            const userId = mentionedUser.id;
+            message.reply({
+                content: `MAS PRA JOGAR LOL VOCÊ É BOM <@${userId}> `,
+                files: ['image.png']
+            });
+        } else {
+            message.reply("Você precisa mencionar um usuário após o comando /lol.");
+        }
+    }
+});
+
+client.on('ready', (e) => {
+    console.log(`${e.user.tag} esta Online!`);
+})
+
+client.login(process.env.TOKEN)
